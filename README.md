06a9232811d6f2f75ff6a0325ab39c5815c0d2ff
lauren levaccare <laurenlevaccare@icloud.com>
2021-02-27 14:11:19 -0600

    readme file with wireframe

---
+++ README.md
@@ -0,0 +1,3 @@
+* React Express MySQL Todo's
+
+*[Wireframe](https://github.com/loriculberson/react-express-mysql-todos)
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoApp gitfrom "./TodoApp"
+import TodoApp from "./TodoApp"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
