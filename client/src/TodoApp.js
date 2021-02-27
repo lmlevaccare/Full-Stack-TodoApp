26beb827506cf900255e52d81f53337b88bd21e2
lauren levaccare <laurenlevaccare@icloud.com>
2021-02-27 14:00:42 -0600

    first commit

---
+++ client/src/TodoApp.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import "./index.css"
+
+function TodoApp() {
+  return (
+    <div className="container">
+      <h1>Todo List</h1>
+    </div>
+  );
+}
+
+export default TodoApp;
---
+++ client/src/components/TaskListForm.js
@@ -0,0 +1,25 @@
+// import React from 'react'
+// import "../App.css"
+// import TaskDeleteBtn from "./TaskDeleteBtn"
+// const TaskListForm = () => {
+ 
+
+//     return (
+//         <form className="container">
+//             <div className="">
+//                 <label>Add Todo List</label>
+//                 <input
+//                     type="text"
+//                     placeholder="Add Todo"></input>
+//                 {/* //   value={Name}
+//         //   onChange={(e) => setName(e.target.value)} */}
+//            <input 
+//      type="submit"
+//      value="Submit"       
+//                 />
+//                 <Task
+//             </div>
+//              </form>
+//     )
+// }
+// export default TaskListForm;
